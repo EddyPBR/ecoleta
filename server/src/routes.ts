d62e853aed8fb328523bdcfa51ecd8aeb5c6df69
EddyPBR <edvaldojunior1310@gmail.com>
2020-06-02 14:43:18 -0300

    criando as rotas de controle da api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
-const routes = express.Router();
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "hello world" });
-});
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get("/items", itemsController.index);
+routes.post('/points', pointsController.create);
 
 export default routes;
