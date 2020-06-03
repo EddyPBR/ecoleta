9b3c3603f0ebcaccacefeeef355aa4eb00620813
EddyPBR <edvaldojunior1310@gmail.com>
2020-06-03 11:23:34 -0300

    preparando as rotas com react-router-dom

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { FiLogIn } from 'react-icons/fi'
+import { Link } from "react-router-dom";
+import { FiLogIn } from "react-icons/fi";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
 
@@ -13,14 +14,16 @@ const Home = () => {
 
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
-          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
 
-          <a href="/cadastro">
+          <Link to="/cadastro">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>
